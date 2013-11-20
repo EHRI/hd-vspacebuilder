e6559758ee457f8d13899d17797009c8c8d4828a
KepaJRodriguez <kepa.rodriguez@gmail.com>
2013-11-20 17:43:05 +0100

    to be followed

--- src/main/java/eu/ehri/hd_vspacebuilder/App.java
+++ src/main/java/eu/ehri/hd_vspacebuilder/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
+    	
+    	String folderName = args[0];
+    	LoadFiles upload = new LoadFiles();
+    	
+    	
         System.out.println( "Hello World!" );
     }
 }
