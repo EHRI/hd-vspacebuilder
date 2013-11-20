092fb5c0735f15827cb6bc594b56a505a1dd4e34
KepaJRodriguez <kepa.rodriguez@gmail.com>
2013-11-20 18:18:06 +0100

    Initial workflow

--- src/main/java/eu/ehri/hd_vspacebuilder/App.java
+++ src/main/java/eu/ehri/hd_vspacebuilder/App.java
@@ -1,18 +1,49 @@
 package eu.ehri.hd_vspacebuilder;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import org.annolab.tt4j.TreeTaggerException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException, TreeTaggerException
     {
     	
     	String folderName = args[0];
+
+    	//Load collection and file descriptions
     	LoadFiles upload = new LoadFiles();
-    	
-    	
-        System.out.println( "Hello World!" );
+		HashMap<String, List<String>> uploaded = upload
+				.loadFiles4Model(folderName);
+		
+		//Tokenize the texts
+		TokenizeLearnMaterial tokenizeMaterial = new TokenizeLearnMaterial();
+		HashMap<String, List<String>> tokenized = tokenizeMaterial
+				.tokenizeDescriptions(uploaded);
+		uploaded.clear();
+		
+		//Annotate part of speech
+		POSannotateLernMaterial annotatePOS = new POSannotateLernMaterial();
+		HashMap<String, List<String>> annotated = annotatePOS.annotatePOS(tokenized);
+		
+		//Extract relevant features for each institution
+		//Infer the features of the vector space
+		ExtractRelevantFeatures extractFeats = new ExtractRelevantFeatures();
+		HashMap<String, List<String>> extracted = extractFeats
+				.extractFeatures(annotated);
+		
+		//Build vectorial representation of each institution with TF*IDF
+		RankTfIdf rankFeats = new RankTfIdf();
+		HashMap<String, HashMap<String, Double>> tfidf = rankFeats
+				.rankingTfIdf(extracted);
+		
+		//END
+        System.out.println( "FINISH!" );
     }
 }
