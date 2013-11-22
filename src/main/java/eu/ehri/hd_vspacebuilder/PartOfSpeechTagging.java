ab6c9aee389eb0817159e1ee5e5fe366299de15d
KepaJRodriguez <kepa.rodriguez@gmail.com>
2013-11-22 17:21:59 +0100

    Minor modifications

--- src/main/java/eu/ehri/hd_vspacebuilder/PartOfSpeechTagging.java
+++ src/main/java/eu/ehri/hd_vspacebuilder/PartOfSpeechTagging.java
@@ -15,6 +15,7 @@ public class PartOfSpeechTagging {
 	static GetProperties property = new GetProperties();
 	final String treeTaggerFolder = property.getTreeTaggerFolder();
 	
+	
 	public List<String> annotatePOSpeech(List<String> tokenized)
 			throws IOException, TreeTaggerException {
 		// Point TT4J to the TreeTagger installation directory. The executable
@@ -22,27 +23,21 @@ public class PartOfSpeechTagging {
 		// in the "bin" subdirectory - in this example at
 		// "/opt/treetagger/bin/tree-tagger"
 		
-		
-		
-		//System.setProperty("treetagger.home", "/home/kepa/Software/TreeTager/");
 		System.setProperty("treetagger.home", treeTaggerFolder);
 		TreeTaggerWrapper<String> tt = new TreeTaggerWrapper<String>();
 		final List<String> outputTreeTagger = new ArrayList<String>();
 
 		try {
-//			tt.setModel(treeTaggerFolder + "lib/english.par:utf8");
 			tt.setModel(treeTaggerFolder + "lib/english.par:utf8");
 			tt.setHandler(new TokenHandler<String>() {
 				public void token(String token, String pos, String lemma) {
 					outputTreeTagger.add(token + "\t" + pos + "\t" + lemma);
-					// System.out.println(token + "\t" + pos + "\t" + lemma);
 				}
 			});
 			tt.process(tokenized);
 		} finally {
 			tt.destroy();
 		}
-		// System.out.println("SIZE: " + outputTreeTagger.size());
 		return outputTreeTagger;
 	}
 
